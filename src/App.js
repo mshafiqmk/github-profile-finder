dab6bfd2244743a0e6e2b4bbb8b58dc6d4de9041
shafiq khuidad <shafiqkhuidad@gmail.com>
2019-05-27 01:16:25 +0500

    added repos,gits,follower and followings

--- src/App.js
+++ src/App.js
@@ -27,12 +27,18 @@ class App extends React.Component {
     let {dataLoaded,response,responseData} = this.state;
     let resultedProfile = dataLoaded ===true & response ===200 
                           ? <div className="card-deck">
-                              <div className="card">
+                              <div className="card" style={{width:'400px'}}>
                                 <img className="" src={responseData.avatar_url} alt="Github Profile Image" />
                                 <div className="card-body">
                                   <h5 className="card-title">{responseData.name}</h5>
-                                  <p className="card-text">{responseData.bio}</p>
-                                  <p className="card-text"><small class="text-muted">{responseData.location}</small></p>
+                                  <p className="card-text">Bio : {responseData.bio}</p>
+                                  <p className="card-text">Location : {responseData.location}</p>
+                                  <p className="card-text"><small class="text-muted">Public Repos : {responseData.public_repos}</small></p>
+                                  <p className="card-text"><small class="text-muted">Public Gists : {responseData.public_gists}</small></p>
+                                  <p className="card-text"><small class="text-muted">Public Gists : {responseData.public_gists}</small></p>
+                                  <p className="card-text"><small class="text-muted">Followers : {responseData.followers}</small></p>
+                                  <p className="card-text"><small class="text-muted">Following : {responseData.following}</small></p>
+                                  <p className="card-text"><small class="text-muted">Following : {responseData.following}</small></p>
                                 </div>
                               </div>
                             </div>
@@ -51,7 +57,7 @@ class App extends React.Component {
           </form>
         </header>
         <br/>
-        <div className="col-md-4 col-in-center">
+        <div className="col-md-3 col-in-center">
           <div >
             { resultedProfile }
           </div>
